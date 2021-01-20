0a162ad376bc151de359ab6ec02bee71509bd8ef
Firmansyah Kurniawan <kurniawan.firman94@gmail.com>
2021-01-20 17:38:39 +0700

    Commit

---
+++ pb.py
@@ -0,0 +1,20 @@
+import http.client
+
+token = str(input("Masukkan Token: "))
+cookie = str(input("Masukkan Token: "))
+conn = http.client.HTTPSConnection("topup.pointblank.id")
+headers = {
+  'Accept': 'application/json, text/javascript, */*; q=0.01',
+  '__RequestVerificationToken': token,
+  'Content-Type': 'application/json; charset=UTF-8',
+  'Cookie': cookie
+}
+with open("pb.txt", "r") as file:
+    for i in file:
+        payload = '{\"couponno\":\"'+i+'"}'
+        
+while True:
+    conn.request("POST", "/Coupon/Register", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
